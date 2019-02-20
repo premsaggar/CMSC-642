9f4424251985ecce0c257be325cfc3024670f10f
holt15004 <james@holt.net>
2019-02-19 21:39:54 -0500

    first implementation of puzzle methods - recursive and non-recursive.

--- CMSC-624/src/main/java/CMSC_642/CMSC_624/App.java
+++ CMSC-624/src/main/java/CMSC_642/CMSC_624/App.java
@@ -9,5 +9,44 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        System.out.println( "non-recursive" );
+
+        printPuzzle(3,2,3);
+        printPuzzle(4,3,2);
+        printPuzzle(3,4,4);
+
+        System.out.println( "recursive" );
+
+        printPuzzleRecursive(3,2,3);
+        printPuzzleRecursive(4,3,2);
+        printPuzzleRecursive(3,4,4);
+
     }
+    
+    public static void printPuzzle(int a, int b, int c)
+    {
+    	int prev = a; 
+    	for (int i = 0; i < c; i++) {
+    		//System.out.println("fooooo");
+    		prev = prev * b; 
+    		System.out.print(prev);
+    		System.out.print(" ");
+    	}
+    	System.out.println();
+    }
+
+    public static void printPuzzleRecursive(int a, int b, int c)
+    {
+    	int num = a * b; 
+    	System.out.print(num);
+    	System.out.print(" ");
+    	if (c>1) {
+    		printPuzzleRecursive(num,b,c-1);
+    	} else {
+    		System.out.println();
+    	}
+    		
+    }
+
 }
